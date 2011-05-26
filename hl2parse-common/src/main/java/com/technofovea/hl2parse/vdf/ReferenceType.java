3ba10f046a8b0b84204d6c068997b1a9fd8fecd2
dhager <darien+commits@technofovea.com>
2011-05-25 18:49:34 -0700

    Added missing license headers

--- hl2parse-common/src/main/java/com/technofovea/hl2parse/vdf/ReferenceType.java
+++ hl2parse-common/src/main/java/com/technofovea/hl2parse/vdf/ReferenceType.java
@@ -1,5 +1,14 @@
-/*
- * 
+/**
+ * Copyright (C) 2011 Darien Hager
+ *
+ * This code is part of the "HL2Parse" project, and is licensed under
+ * a Creative Commons Attribution-ShareAlike 3.0 Unported License. For
+ * either a summary of conditions or the full legal text, please visit:
+ *
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ * Permissions beyond the scope of this license may be available
+ * at http://technofovea.com/ .
  */
 package com.technofovea.hl2parse.vdf;
 
